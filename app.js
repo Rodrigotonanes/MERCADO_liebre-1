9eab544485a3396a4a518ee4666b0f883abc9be4
Rodrigo Toñanes <rodrigotonanes030@gmail.com>
2024-01-12 15:07:47 -0300

    .

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public/css'))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/home.html"));
